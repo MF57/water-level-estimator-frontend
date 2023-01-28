a5b92984902ccac3d6aaae667712bd47049edb30
Piotr Bochenek <p-bochenek@outlook.com>
2023-01-28 17:53:57 +0100

    backend integration

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {Observable, of} from "rxjs";
+import {map, Observable, of} from "rxjs";
 import {HttpClient, HttpParams} from "@angular/common/http";
 
 @Injectable({providedIn: 'root'})
@@ -10,50 +10,47 @@ export class DataService {
   public getData(date: string): Observable<any> {
     let params = new HttpParams().set('date', date);
 
-    this.client.get("https://miedz.tegess.com/dupa", {params, responseType: 'json'}).subscribe(data => {
-      console.log(data);
-    })
+    return this.client.get<{date: string, value: number}[]>("https://miedz.tegess.com/dupa", {params, responseType: 'json'}).pipe(
+      map(data => {
+        return {// values on X-Axis
+          labels: data.map(obj => obj.date),
+          datasets: [
+            {
+              label: "Poziom Wody",
+              data: data.map(obj => obj.value),
+              borderColor: 'white',
+              cubicInterpolationMode: 'monotone',
+              pointStyle: 'circle',
+              pointRadius: 10,
+              pointHoverRadius: 15,
+              backgroundColor: function(context: any) {
+                let width, height, gradient;
+                const chart = context.chart;
+                const {ctx, chartArea} = chart;
 
+                if (!chartArea) {
+                  // This case happens on initial chart load
+                  return;
+                }
+                const chartWidth = chartArea.right - chartArea.left;
+                const chartHeight = chartArea.bottom - chartArea.top;
+                if (!gradient || width !== chartWidth || height !== chartHeight) {
+                  // Create the gradient because this is either the first render
+                  // or the size of the chart has changed
+                  width = chartWidth;
+                  height = chartHeight;
+                  gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
+                  gradient.addColorStop(0, "rgba(0,0,0,0)");
+                  gradient.addColorStop(1, "#f19551");
+                }
 
-    return of({// values on X-Axis
-      labels: ['2022-05-10', '2022-05-11', '2022-05-12', '2022-05-13',
-        '2022-05-14', '2022-05-15', '2022-05-16', '2022-05-17',],
-      datasets: [
-        {
-          label: "Poziom Wody",
-          data: ['467', '576', '572', '79', '92',
-            '574', '520', '430'],
-          borderColor: 'white',
-          cubicInterpolationMode: 'monotone',
-          pointStyle: 'circle',
-          pointRadius: 10,
-          pointHoverRadius: 15,
-          backgroundColor: function(context: any) {
-            let width, height, gradient;
-            const chart = context.chart;
-            const {ctx, chartArea} = chart;
-
-            if (!chartArea) {
-              // This case happens on initial chart load
-              return;
-            }
-            const chartWidth = chartArea.right - chartArea.left;
-            const chartHeight = chartArea.bottom - chartArea.top;
-            if (!gradient || width !== chartWidth || height !== chartHeight) {
-              // Create the gradient because this is either the first render
-              // or the size of the chart has changed
-              width = chartWidth;
-              height = chartHeight;
-              gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
-              gradient.addColorStop(0, "rgba(0,0,0,0)");
-              gradient.addColorStop(1, "#f19551");
-            }
-
-            return gradient;
-          },
-          fill: true
-        },
-      ]
-    })
+                return gradient;
+              },
+              fill: true
+            },
+          ]
+        }
+      })
+    )
   }
 }
