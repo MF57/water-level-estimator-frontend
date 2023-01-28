2afa8d8a4b2c0a0ff23e5af0a702c2673b68ff96
Piotr Bochenek <p-bochenek@outlook.com>
2023-01-28 19:44:58 +0100

    error

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,43 +10,35 @@ export class DataService {
   public getData(date: string): Observable<any> {
     let params = new HttpParams().set('date', date);
 
-    return this.client.get<{date: string, value: number}[]>("https://miedz.tegess.com/dupa", {params, responseType: 'json'}).pipe(
+    return this.client.get<{date: string, value: number, error: number}[]>("https://miedz.tegess.com/dupa", {params, responseType: 'json'}).pipe(
       map(data => {
         return {// values on X-Axis
           labels: data.map(obj => obj.date),
+
           datasets: [
             {
-              label: "Poziom Wody",
+              label: 'Minimalna niepewność',
+              data: data.map(obj => obj.value - obj.error),
+              borderColor: '#f19551',
+              cubicInterpolationMode: 'default',
+              fill: true,
+            },
+            {
+              label: "Przewidywany poziom Wody",
               data: data.map(obj => obj.value),
               borderColor: 'white',
-              cubicInterpolationMode: 'monotone',
+              cubicInterpolationMode: 'default',
               pointStyle: 'circle',
               pointRadius: 10,
               pointHoverRadius: 15,
-              backgroundColor: function(context: any) {
-                let width, height, gradient;
-                const chart = context.chart;
-                const {ctx, chartArea} = chart;
-
-                if (!chartArea) {
-                  // This case happens on initial chart load
-                  return;
-                }
-                const chartWidth = chartArea.right - chartArea.left;
-                const chartHeight = chartArea.bottom - chartArea.top;
-                if (!gradient || width !== chartWidth || height !== chartHeight) {
-                  // Create the gradient because this is either the first render
-                  // or the size of the chart has changed
-                  width = chartWidth;
-                  height = chartHeight;
-                  gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
-                  gradient.addColorStop(0, "rgba(0,0,0,0)");
-                  gradient.addColorStop(1, "#f19551");
-                }
-
-                return gradient;
-              },
-              fill: true
+            },
+            {
+              label: 'Maksymalna niepewność',
+              data: data.map(obj => obj.value + obj.error),
+              borderColor: '#f19551',
+              cubicInterpolationMode: 'default',
+              fill: "-2",
+              backgroundColor: 'rgba(241,149,81,0.56)',
             },
           ]
         }
