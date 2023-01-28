8cc3e29250eb71bb84bcfbe53024e5ea2de9be4a
Piotr Bochenek <p-bochenek@outlook.com>
2023-01-28 15:18:09 +0100

    add service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import Chart from 'chart.js/auto';
+import {DataService} from "./data.service";
 
 
 @Component({
@@ -9,38 +10,23 @@ import Chart from 'chart.js/auto';
 })
 export class AppComponent {
   public chart: any;
+  public baseHref = window.location;
+
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.createChart();
+    this.dataService.getData().subscribe(data => {
+      this.createChart(data);
+    })
   }
 
-  createChart() {
-
+  createChart(data: any) {
     this.chart = new Chart("MyChart", {
       type: 'bar', //this denotes tha type of chart
-
-      data: {// values on X-Axis
-        labels: ['2022-05-10', '2022-05-11', '2022-05-12', '2022-05-13',
-          '2022-05-14', '2022-05-15', '2022-05-16', '2022-05-17',],
-        datasets: [
-          {
-            label: "Sales",
-            data: ['467', '576', '572', '79', '92',
-              '574', '573', '576'],
-            backgroundColor: 'blue'
-          },
-          {
-            label: "Profit",
-            data: ['542', '542', '536', '327', '17',
-              '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
-          }
-        ]
-      },
+      data,
       options: {
         aspectRatio: 2.5
       }
-
     });
   }
 
